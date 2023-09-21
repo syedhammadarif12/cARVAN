f59911894197e8f2e386b8a43519a3a5ecc186a2
syedhammadarif12 <132674378+syedhammadarif12@users.noreply.github.com>
2023-09-21 15:52:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+searchBox.addEventListener("click", ()=>{
+  navbar.classList.toggle("showInput");
+  if(navbar.classList.contains("showInput")){
+    searchBox.classList.replace("bx-search" ,"bx-x");
+  }else {
+    searchBox.classList.replace("bx-x" ,"bx-search");
+  }
+});
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
+
+
+
+
+
+
+
+
+// Testimonials carousel
+$(".testimonial-carousel").owlCarousel({
+  autoplay: true,
+  smartSpeed: 1000,
+  center: true,
+  margin: 25,
+  dots: true,
+  loop: true,
+  nav : false,
+  responsive: {
+      0:{
+          items:1
+      },
+      768:{
+          items:2
+      },
+      992:{
+          items:3
+      }
+  }
+});
